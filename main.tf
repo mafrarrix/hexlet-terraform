913d9a5cdb2a92b222129fdd504f95e4a6055c45
mafrarrix <devapp.vs@outlook.com>
2024-05-12 23:49:06 +0200

    Config digitalocean

--- main.tf
+++ main.tf
@@ -17,3 +17,33 @@ provider "digitalocean" {
 data "digitalocean_ssh_key" "terraform" {
   name = "hexlet-vm"
 }
+
+resource "digitalocean_droplet" "default"{
+  image = "ubuntu-20-04-x64"
+  name = "www-1"
+  region = "fra1"
+  size = "s-2vcpu-2gb"
+  ssh_keys = [
+      data.digitalocean_ssh_key.terraform.name
+    ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx"
+    ]
+  }
+}
+
+
+
